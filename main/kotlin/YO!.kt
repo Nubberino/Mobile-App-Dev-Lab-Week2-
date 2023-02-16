30cd99e4fe68d08b8635d5732996dedfe1d59de0
Nubberino <ignatius.sarwono@student.umn.ac.id>
2023-02-16 11:13:41 +0700

    Learning Why EVERYTHING has value
    lol

--- main/kotlin/YO!.kt
+++ main/kotlin/YO!.kt
@@ -1,4 +1,18 @@
-fun main(args: Array<String>)
+//fun main(args: Array<String>)
+//{
+//    println("Yo ${args[0]}")
+//}
+
+fun main()
 {
-    println("Yo ${args[0]}")
+//    val isUnity = println("Joewari Da")
+    val isUnit = "Joewari Da"
+    println(isUnit)
+
+    val temp = 21
+    val SuperHot = if (temp < 50) true else false
+//    println(SuperHot)
+
+    val Message = "The temp is ${if (temp < 50) "aight" else "Ay yo Chill"}"
+    println(Message)
 }
