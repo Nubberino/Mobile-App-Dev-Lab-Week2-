2eaedb0f4790b5d2738218d02fbed4e28da43f8f
Nubberino <ignatius.sarwono@student.umn.ac.id>
2023-02-16 11:32:54 +0700

    Learning more about functions

--- main/kotlin/YO!.kt
+++ main/kotlin/YO!.kt
@@ -1,3 +1,7 @@
+import java.util.*
+
+
+
 //fun main(args: Array<String>)
 //{
 //    println("Yo ${args[0]}")
@@ -6,13 +10,45 @@
 fun main()
 {
 //    val isUnity = println("Joewari Da")
-    val isUnit = "Joewari Da"
-    println(isUnit)
-
-    val temp = 21
-    val SuperHot = if (temp < 50) true else false
+//    val isUnit = "Joewari Da"
+//    println(isUnit)
+//
+//    val temp = 21
+//    val SuperHot = if (temp < 50) true else false
 //    println(SuperHot)
+//
+//    val Message = "The temp is ${if (temp < 50) "aight" else "Ay yo Chill"}"
+//    println(Message)
 
-    val Message = "The temp is ${if (temp < 50) "aight" else "Ay yo Chill"}"
-    println(Message)
+
+    TargetInt()
+    TargetInt()
+    TargetInt()
 }
+
+fun randomLane(): String
+{
+    val loc = arrayOf("Top","Mid","Jgl","Bot")
+    return loc[Random().nextInt(loc.size)]
+}
+
+fun IntTarget(lane : String) :String
+{
+
+   return when(lane)
+    {
+        "Top" -> "Top Laner"
+        "Mid" -> "Mid Laner"
+        "Jgl" -> "Jungler"
+        "Bot" -> "ADC"
+        else -> "NO!!!!!!"
+    }
+}
+
+fun TargetInt()
+{
+    val len = randomLane()
+    val fud = IntTarget(len)
+    println("I will feed $len's $fud")
+}
+
